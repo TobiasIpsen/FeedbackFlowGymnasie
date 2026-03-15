c65027ea12826d852c883a9a0806e4ebf8f1813d
Tobias Ipsen <71149806+TobiasIpsen@users.noreply.github.com>
2026-03-15 13:31:58 +0100

    feat(question): add missing enums

--- feedbackFlowAPI/Helpers/Enums.cs
+++ feedbackFlowAPI/Helpers/Enums.cs
@@ -7,27 +7,88 @@ namespace feedbackFlowAPI.Helpers
         A,
         B,
         C,
-        D,
-        E,
-        F
+        D
     }
 
-    public enum QuestionType
+    /// <summary>
+    /// Delprøve. Digital eller Analog
+    /// </summary>
+    public enum ExamType
     {
         Digital,
         Analog
     }
 
-    public enum UserRole
-    {
-        Student,
-        Teacher,
-        Admin
-    }
-
+    /// <summary>
+    /// Is used for question answers. Private, Public
+    /// </summary>
     public enum Visibility
     {
         Private,
         Public
+    }
+
+    public enum QuestionDifficulty
+    {
+        Hard,
+        Medium,
+        Easy
+    }
+
+    /// <summary>
+    /// If a question needs sopmething specific or not.
+    /// ApplyFormula (Anvend formel), 
+    /// SpecificMethod (Bestemt metode),
+    /// NoRequirement (Ingen krav)
+    /// </summary>
+    public enum QuestionMethodRequirement
+    {
+        ApplyFormula,
+        SpecificMethod,
+        NoRequirement
+    }
+
+    /// <summary>
+    /// Uddannelse.
+    /// HF, HHX, HTX, STX
+    /// </summary>
+
+    public enum Education
+    {
+        HF,
+        HHX,
+        HTX,
+        STX
+    }
+
+    /// <summary>
+    /// Kontekst til et spørgsmål. F.eks bare løs opgaven eller der er tekst (stor/lille) tilhørende opgave.
+    /// </summary>
+    public enum QuestionContext
+    {
+        YesHeavy,
+        YesLight,
+        No
+    }
+
+    /// <summary>
+    /// Om der er et tvist i opgaven eller ej.
+    /// Ja, Delvist, Nej, Med et tvist.
+    /// </summary>
+    public enum StandardQuestion
+    {
+        Yes,
+        Partially,
+        No,
+        WithATwist
+    }
+
+    /// <summary>
+    /// Ny-gammel ordning. Det er ift. bekendtgørelser
+    /// </summary>
+    public enum NewOldSystem
+    {
+        New,
+        Old
     }
 }
