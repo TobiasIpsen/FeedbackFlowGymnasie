fe03cd7bce8d6c5612b35bbdffc0fb8385447c1f
L3wis76 <lewistranvu190@gmail.com>
2026-04-08 01:19:59 +0200

    Fejltyper til elever

--- feedbackFlowAPI/Services/Interfaces/IStudentResultsService.cs
+++ feedbackFlowAPI/Services/Interfaces/IStudentResultsService.cs
@@ -7,5 +7,8 @@ namespace feedbackFlowAPI.Services.Interfaces
     public interface IStudentResultsService
     {
         public Task<TeacherFeedbackDTO> SaveTeacherFeedbackAsync(int studentId, int questionSetId, int questionId, TeacherFeedbackDTO dto);
+        public Task<List<MistakeTypeDTO>> GetMistakeTypesAsync();
+        public Task<List<MistakeTypeDTO>?> GetAssignedMistakesAsync(int studentId, int questionSetId, int questionId, int teacherId);
+        public Task<(bool Success, string? Error)> AssignMistakesAsync(int studentId, int questionSetId, int questionId, AssignMistakesDTO dto);
     }
 }
