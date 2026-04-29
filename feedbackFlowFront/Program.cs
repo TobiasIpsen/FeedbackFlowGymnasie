54754a5c5ca66a829a415f1f7c719bce24144f0c
L3wis76 <143950236+L3wis76@users.noreply.github.com>
2026-04-29 12:32:37 +0200

    front

--- feedbackFlowFront/Program.cs
+++ feedbackFlowFront/Program.cs
@@ -19,6 +19,8 @@ namespace feedbackFlowFront
             builder.Services.AddScoped<SelfAssessmentService>();
             builder.Services.AddScoped<QuestionService>();
             builder.Services.AddScoped<QuestionSetService>();
+            builder.Services.AddSingleton<QuestionService>();
+
 
             var app = builder.Build();
 
