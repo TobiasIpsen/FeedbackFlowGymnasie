bce2dbbb90dbfa6d0103cc5dc96389b47a8f248e
Tobias Ipsen <71149806+TobiasIpsen@users.noreply.github.com>
2026-06-23 21:29:34 +0200

    wip: missing

---
+++ feedbackFlowAPI/DTOs/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿using feedbackFlowAPI.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace feedbackFlowAPI.DTOs;
+
+public partial class UserDTO
+{
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public UserDTO(User entity)
+    {
+        this.Firstname = entity.Firstname;
+        this.Lastname = entity.Lastname;
+        this.Email = entity.Email;
+    }
+
+}
